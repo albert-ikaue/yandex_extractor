46238e19e7f99231af9f7bc14846a936721ddd09
Julio María Meca Hansen <julio@ikaue.com>
2021-04-26 10:16:59 +0200

    1rst commmit

---
+++ YANDEX_EXTRACTOR/bin/yandex_extractor.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# (C) 2018 IKAUE, Marketing de Optimizacion
+# created by Albert Moral Lleó <albert@ikaue.com>
+
+"""
+This module generates uploads to BQ a table with the data extracted from yandex webmaster API.
+"""
+
+
---
+++ setup_common.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages
+import NLP_ikaue
+
+def define_args():
+    install_requirements = ['beautifulsoup4>=4.9.1','wheel>=0.36.2','googlesearch-python>=2020.0.2',
+                            'google-cloud-language>=2.0.0','pandas>=1.2.4','pytest>=6.2.3']
+
+    test_requirements = ['pytest>=4.3,<4.4', 'pytest-cov>=2.7,<2.8']
+    args = {
+        'name': 'NLP_ikaue Python3 IKAUE',
+        'version': NLP_ikaue.__version__,
+        'url': 'https://github.com/albertlleo/python3_ikaue',
+        'author': 'Albert Lleo',
+        'author_email': 'albert@ikaue.com',
+        'classifiers': ['Development Status :: 3 - Alpha', 'Programming Language :: Python :: 3'],
+        'packages':  find_packages(),
+        'python_requires': '>=3.5',
+        'entry_points': {'console_scripts': ['']},
+        'install_requires': install_requirements,
+        'extras_require': {'testing': test_requirements},
+        'project_urls': {'Source':
+                         'https://github.com/albertlleo/python3_ikaue'}
+
+    }
+
+    return args
