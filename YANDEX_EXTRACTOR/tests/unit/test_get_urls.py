d29d1a2e98a88232d90e232d6dd4fa176149d3df
albertlleo <albert.lleo3@gmail.com>
2021-04-29 10:22:40 +0200

    Fixing imports and tests

--- YANDEX_EXTRACTOR/tests/unit/test_get_urls.py
+++ YANDEX_EXTRACTOR/tests/unit/test_get_urls.py
@@ -7,7 +7,6 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../../src')
 from yandex_extractor import date_range
 
-
 @pytest.mark.parametrize(
     ["expected"],
     [
